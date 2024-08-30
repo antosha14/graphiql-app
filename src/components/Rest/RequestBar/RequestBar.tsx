d707ae9410f89ae2738b97597d17efb28b6bf946
unknown <anton.kozel.97@mail.ru>
2024-08-30 20:38:52 +0300

    feat: add base64 encoded method, url and body to the url

--- src/components/Rest/RequestBar/RequestBar.tsx
+++ src/components/Rest/RequestBar/RequestBar.tsx
@@ -1,43 +1,101 @@
 'use client';
 
 import styles from './RequestBar.module.scss';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { json } from '@codemirror/lang-json';
 import { ApexTheme } from '@models/codeMirrorTheme';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 
-const requestTypeOptions = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'];
+const requestTypeOptions = [
+  { method: 'GET', color: '#90EE90' },
+  { method: 'POST', color: '#FBBF00' },
+  { method: 'PUT', color: '#00A1E0' },
+  { method: 'DELETE', color: '#D9534F' },
+  { method: 'PATCH', color: '#FFB74D' },
+  { method: 'OPTIONS', color: '#9E9E9E' },
+  { method: 'HEAD', color: '#673AB7' },
+  { method: 'CONNECT', color: '#FF4081' },
+  { method: 'TRACE', color: '#3F51B5' },
+];
 const initialBodyText = '{\n  "message": "Write request body here"\n}';
 
 export default function RequestBar() {
-  const [code, setCode] = useState<string>(initialBodyText);
+  const pathname = usePathname();
+  const urlParts = pathname.split('/').slice(2);
+  const [method, encodedUrl = '', encodedBody = ''] = urlParts;
+
+  const [requestMethod, setRequestMethod] = useState<string>(method || 'GET');
+
+  const [requestBody, setRequestBody] = useState<string>(encodedBody ? atob(encodedBody) : initialBodyText);
+
+  const [url, setUrl] = useState<string>(encodedUrl ? atob(encodedUrl) : 'noUrl');
+  const urlInputRef = useRef<HTMLInputElement>(null);
+  const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout | null>(null);
+
   const editorRef = useRef(null);
   const router = useRouter();
+
+  // const searchParams = useSearchParams();
+  // const slug = searchParams.get('slug');
+
+  useEffect(() => {
+    if (urlInputRef.current && url !== 'noUrl') {
+      urlInputRef.current.focus();
+    }
+  }, [url]);
+
+  const updateUrl = (method: string, urlValue: string, bodyValue: string) => {
+    const encodedUrl = btoa(urlValue);
+    const encodedBody = btoa(bodyValue);
+    router.push(`/restful/${method}/${encodedUrl}/${encodedBody}`);
+  };
 
   const handlePrettifyClick = () => {
     if (editorRef.current) {
       try {
-        const jsonObject = JSON.parse(code);
+        const jsonObject = JSON.parse(requestBody);
         const prettyJson = JSON.stringify(jsonObject, null, 2);
-        setCode(prettyJson);
+        setRequestBody(prettyJson);
       } catch {
-        setCode('Invalid JSON input');
+        setRequestBody("Your input wasn't a valid JSON");
       }
     }
   };
 
+  const handleBodyBlur = () => {
+    try {
+      updateUrl(requestMethod, url, requestBody);
+    } catch {
+      setRequestBody("Your input wasn't a valid JSON");
+    }
+  };
+
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setUrl(value);
+    if (typingTimeout) {
+      clearTimeout(typingTimeout);
+    }
+    setTypingTimeout(
+      setTimeout(() => {
+        updateUrl(requestMethod, value, requestBody);
+      }, 1000)
+    );
+  };
+
   const handleMethodChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    router.push(`/restful/${e.target.value}`);
+    setRequestMethod(e.target.value);
+    updateUrl(e.target.value, url, requestBody);
   };
 
   const handleBodyChange = (value: string) => {
-    setCode(value);
+    setRequestBody(value);
   };
 
   const handleCopyClick = () => {
     if (editorRef.current) {
-      navigator.clipboard.writeText(code);
+      navigator.clipboard.writeText(requestBody);
     }
   };
 
@@ -45,18 +103,36 @@ export default function RequestBar() {
     <div className={styles['query-section']}>
       <div className={styles['query-bar-container']}>
         <div className={styles['method-select']}>
-          <select name="method" className={styles.methodSelector} onChange={handleMethodChange}>
-            {requestTypeOptions.map(method => {
+          <select
+            name="method"
+            className={styles.methodSelector}
+            style={{ color: `${requestTypeOptions.find(option => option.method === requestMethod)?.color}` }}
+            onChange={handleMethodChange}
+            value={requestMethod}
+          >
+            {requestTypeOptions.map(value => {
               return (
-                <option key={method} value={method} className={styles.methodOption}>
-                  {method}
+                <option
+                  key={value.method}
+                  value={value.method}
+                  style={{ color: `${value.color}` }}
+                  className={styles.methodOption}
+                >
+                  {value.method}
                 </option>
               );
             })}
           </select>
         </div>
         <div className={styles['url-input']}>
-          <input type="text" className={styles.urlInputField} placeholder="Enter URL or paste text" />
+          <input
+            ref={urlInputRef}
+            type="text"
+            className={styles.urlInputField}
+            placeholder="Enter URL or paste text"
+            value={url !== 'noUrl' ? url : ''}
+            onChange={handleUrlChange}
+          />
         </div>
         <div className={styles.sendButtonContainer}>
           <button className={styles['send-button']}>Send</button>
@@ -68,13 +144,14 @@ export default function RequestBar() {
         </div>
         <div className={styles.bodyEditorContainer}>
           <CodeMirror
-            value={code}
+            value={requestBody}
             extensions={[json()]}
             theme={ApexTheme}
             className={styles.codeMirror}
             height="200px"
             ref={editorRef}
             onChange={value => handleBodyChange(value)}
+            onBlur={handleBodyBlur}
           />
         </div>
         <div className={styles.buttonsContainer}>
